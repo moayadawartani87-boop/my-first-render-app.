d62f75c88a3731d0b96f57f1252dadaa30c61c26
Moayad Awartani <moayad.awartani87@gmail.com>
2025-12-26 18:57:53 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>مرحباً بك في تطبيقي الأول على Render!</h1><p>تم النشر بنجاح.</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+
