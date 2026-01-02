75b0846978c088831deccdfb6087fe6199963470
Moayad Awartani <moayad.awartani87@gmail.com>
2026-01-02 19:10:41 +0300

    Update streamlit

--- streamlit run app.py
+++ streamlit run app.py
@@ -1,9 +1,4 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-# عرض العنوان الرئيسي
 st.title("مرحباً بك في تطبيقي الأول على Render!")
-
-# عرض نص فرعي
+st.write("تم النشر بنجاح.")
+st.title("مرحباً بك في تطبيقي الأول على Render!")
 st.write("تم النشر بنجاح.")
