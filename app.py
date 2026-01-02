1ae144ce2045d2cf4c636954cd0cf0a41f96e56f
Moayad Awartani <moayad.awartani87@gmail.com>
2026-01-02 21:19:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# عرض العنوان الرئيسي
 st.title("مرحباً بك في تطبيقي الأول على Render!")
+
+# عرض نص فرعي
 st.write("تم النشر بنجاح.")
